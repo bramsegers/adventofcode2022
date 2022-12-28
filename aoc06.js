f94d9c3670ca97e6e973de9b44061d14e25f2974
Abraham Segers <bramsegers@users.noreply.github.com>
2022-12-28 05:24:26 +0100

    Delete aoc06.js

--- aoc06.js
+++
@@ -1,6 +0,0 @@
-let input = require('fs').readFileSync('input/aoc06.txt','utf8')
-
-let index = n => [...input].findIndex((_,i) => new Set(input.slice(i-n,i)).size == n)
-
-console.log( {p1: index( 4)} )
-console.log( {p2: index(14)} )
